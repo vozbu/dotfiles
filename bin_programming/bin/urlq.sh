63dd7d088522a6eb8c9729de0d9f0089f5b63b6d
Valeriy Bykov <valery.bickov@gmail.com>
2026-06-16 17:28:06 +0300

    [+] urlq.sh now can encode and decode urls

--- bin_programming/bin/urlq.sh
+++ bin_programming/bin/urlq.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
-if [ $# -gt 0 ]; then
-    # url encodes parameters
-    echo "$@" | jq -Rr @uri
+usage() {
+    echo "Usage: $0 [mode] [<string>]"
+    echo "Stdin can be used instead of [<string>]"
+    echo "Modes:"
+    echo " -e - encode URL"
+    echo " -d - decode URL"
+}
+
+if [ $# -lt 1 ]; then
+    usage
+    exit 0
+fi
+
+MODE=$1
+case $MODE in
+    "-e") JQ_MODE="@uri" ;;
+    "-d") JQ_MODE="@urid" ;;
+    *   ) echo "Unknown mode $MODE"; usage; exit 1 ;;
+esac
+
+if [ $# -gt 1 ]; then
+    # handle parameters
+    echo "$@" | jq -Rr $JQ_MODE
 else
-    # url encodes stdin
-    cat | jq -Rr @uri
+    # handle stdin
+    cat | jq -Rr $JQ_MODE
 fi
